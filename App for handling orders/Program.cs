29be2799c77364953164b65b9d9ff9830f931eea
GameRuiner <markgolovko@gmail.com>
2019-03-25 13:15:43 +0100

    Add project files.

---
+++ App for handling orders/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_for_handling_orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
